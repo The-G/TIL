b1fb6d9dcdb531f5df8afd8fe4301664a70eae2d
Can <gskjhj@gmail.com>
2024-06-11 22:26:30 +0900

    Just starting Day4 lecture

---
+++ Algorithm_20240607_20240000/Course_1_4_Learned_about_stacks_and_queues.py
@@ -0,0 +1,26 @@
+"""
+
+    4일차: 스택(Stack)과 큐(Queue)
+    
+    학습 목표
+    - 스택과 큐의 기본 개념 이해
+    - 스택과 큐의 주요 연산 이해
+    - 파이썬으로 스택과 큐 구현
+    - 스택과 큐의 활용 사례 이해
+    
+    학습 내용
+    1. 스택(Stack)
+        - 스택의 개념
+        - LIFO(Last In, First Out) 구조
+        - 주요 연산: push, pop, peek, is_empty
+    2. 큐(Queue)
+        - 큐의 개념
+        - FIFO(First In, First Out) 구조
+        - 주요 연산: enqueue, dequeue, front, is_empty
+    
+    실습 문제
+    - 스택을 사용하여 문자열의 괄호 균형을 확인하는 함수 작성
+    - 큐를 사용하여 주어진 수열의 순서를 뒤집는 함수 작성
+
+"""
+
