e8efb857fc055e8eae154b809875dd30bb76df22
Can <gskjhj@gmail.com>
2024-06-12 22:53:15 +0900

    Check the stack part in Day4

--- Algorithm_20240607_20240000/Day4_Learned_about_stacks_and_queues.py
+++ Algorithm_20240607_20240000/Day4_Learned_about_stacks_and_queues.py
@@ -24,3 +24,47 @@
 
 """
 
+# 학습 자료 
+
+# 1. 스택(Stack)
+class Stack:
+    def __init__(self):
+        self.items = [] # 슽택을 저장할 리스트를 초기화함. 
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            item = self.items[-1]  
+            del self.items[-1]     
+            return item            
+    # def pop(self):
+    #     if not self.is_empty():
+    #         return self.items.pop()
+    #     return None 
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None 
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def size(self):
+        return len(self.items)
+
+# 예제 사용 
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop()) # Output: 3 
+print(stack.peek()) # Output: 2
+print(stack.is_empty()) # Output: False
+print(stack.size()) # Output: 2
+
+
