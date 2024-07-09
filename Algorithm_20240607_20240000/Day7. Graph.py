2feb32a6607895d2c5ac266fe019f127d45aa513
Can <gskjhj@gmail.com>
2024-07-09 23:47:23 +0900

    basic graph implementation with DFS and BFS algorithm

--- Algorithm_20240607_20240000/Day7. Graph.py
+++ Algorithm_20240607_20240000/Day7. Graph.py
@@ -29,6 +29,78 @@
 
 """
 
+# 예제코드 
 
+import networkx as nx 
+import matplotlib.pyplot as plt
+
+# Graph 클래스를 정의함. 
+class Graph:
+    def __init__(self):
+        self.graph = {} # 그래프를 저장할 dictionary 생성 
+
+    def add_vertex(self, vertex):
+        # 정점을 그래프에 추가한다. 
+        if vertex not in self.graph:
+            self.graph[vertex] = []
+
+    def add_edge(self, vertex1, vertex2):
+        # 두 정점 간의 간선을 그래프에 추가한다. 무방향 그래프이므로 양쪽에 추가한다. 
+        if vertex1 in self.graph and vertex2 in self.graph:
+            self.graph[vertex1].append(vertex2)
+            self.graph[vertex2].append(vertex1) # 무방향 그래프의 경우 
     
+    def dfs(self, start_vertex, visited=None):
+        # 깊이 우선 탐색(DFS)을 수행한다. 
+        if visited is None:
+            visited = set()
+        visited.add(start_vertex)
+        print(start_vertex, end=' ')
+        for neighbor in self.graph[start_vertex]:
+            if neighbor not in visited:
+                self.dfs(neighbor, visited)
+
+    def bfs(self, start_vertex):
+        # 너비 우선 탐색(BFS)을 수행한다. 
+        visited = set()
+        queue = [start_vertex]
+        visited.add(start_vertex)
+        while queue:
+            vertex = queue.pop(0)
+            print(vertex, end=' ')
+            for neighbor in self.graph[vertex]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+    
+    def visualize(self):
+        G = nx.Graph()
+        for vertex in self.graph:
+            G.add_node(vertex)
+            for neighbor in self.graph[vertex]:
+                G.add_edge(vertex, neighbor)
+
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=2000, font_size=16, font_color='black', edge_color='gray')
+        plt.show()
+    
+# 예제 사용 
+g = Graph()
+vertices = ['A','B','C','D','E']
+for vertex in vertices:
+    g.add_vertex(vertex)
+
+edges = [('A','B'),('A','C'),('B','D'),('C','E'),('A','E')]
+for edge in edges:
+    g.add_edge(edge[0], edge[1])
+
+print("DFS: ")
+g.dfs('A')
+
+print("\n BFS: ")
+g.bfs('A')
+
+# 그래프 시각화 
+g.visualize()
+
 
