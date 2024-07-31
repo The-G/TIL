fea5c8b443672e35ea0fc2b0b9d5ae243c2779c8
The-G <gskjhj@gmail.com>
2024-07-31 23:52:35 +0900

    working on Chapter9_Binary_Search_Tree

--- Algorithm_20240607_20240000/Day9_BST_n_AVLtree.py
+++ Algorithm_20240607_20240000/Day9_BST_n_AVLtree.py
@@ -47,5 +47,73 @@
 # 예제 코드 
 
 ## 이진 탐색 트리 (BST)
+class TreeNode:
+    def __init__(self, key):
+        self.key = key 
+        self.left = None 
+        self.right = None 
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None 
+    
+    def insert(self, key):
+        if self.root is None:
+            self.root = TreeNode(key)
+        else:
+            self._insert(self.root, key)
+        
+    def _insert(self, node, key):
+        if key < node.key:
+            if node.left is None:
+                node.left = TreeNode(key)
+            else:
+                self._insert(node.left, key)
+        else:
+            if node.right is None:
+                node.right = TreeNode(key)
+            else:
+                self._insert(node.right, key)
+
+    def search(self, key):
+        return self._search(self.root, key)
+    
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node 
+        if key < node.key:
+            return self._search(node.left, key)
+        else:
+            return self._search(node.right, key)
+        
+    def delete(self, key): 
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            temp = self._min_value_node(node.right)
+            node.key = temp.key
+            node.right = self._delete(node.right, temp.key)
+        return node
+    
+    def _min_value_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+
 
 ## AVL 트리
+
+
+
